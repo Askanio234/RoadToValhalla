4d998744248bba322f1ce3bbde6beec2267a6025
Artem Chernyaev <askanio234@gmail.com>
2018-01-13 22:46:59 +0300

    Add spawning enemies randomly at 3 positions

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public float speed = 3f;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += Vector3.down * speed * Time.deltaTime;
+	}
+
+}
---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour {
+    public float minTime = 2f;
+    public float maxTime = 6f;
+    public GameObject enemyPrefab;
+
+    private float delayTime;
+    private bool isTimeSet = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isTimeSet)
+        {
+            delayTime = Random.Range(minTime, maxTime);
+            isTimeSet = true;
+        }
+        delayTime -= Time.deltaTime;
+        if(delayTime <= 0)
+        {
+            Instantiate(enemyPrefab, gameObject.transform.position, Quaternion.identity);
+            isTimeSet = false;
+        }
+        
+	}
+    
+}

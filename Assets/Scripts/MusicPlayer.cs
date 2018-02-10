3f61f1dfcf49f7104da801e7a95576ab58e44bec
Artem Chernyaev <askanio234@gmail.com>
2018-02-10 17:31:43 +0300

    Add basic music interface incl volume slider in options

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+    public AudioClip audioClip;
+
+    private AudioSource audioSource;
+	
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    void OnLevelWasLoaded()
+    {
+        audioSource.clip = audioClip;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+
+    public void ChangeVolume(float value)
+    {
+        audioSource.volume = value;
+    }
+}
---
+++ Assets/Scripts/SetStartVolume.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetStartVolume : MonoBehaviour {
+    private MusicPlayer musicPlayer;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        musicPlayer = GameObject.FindObjectOfType<MusicPlayer>();
+        if (musicPlayer)
+        {
+            float volume = PlayerPrefsManager.GetMasterVolume();
+            musicPlayer.ChangeVolume(volume);
+        }
+        else
+        {
+            Debug.LogWarning("Can not find music manager");
+        }
+    }
+}

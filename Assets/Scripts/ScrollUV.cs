569f8e4da2f5246a7b474f1b4e1b39ee026ad359
Artem Chernyaev <askanio234@gmail.com>
2018-02-24 17:13:52 +0300

    Add background scroller

---
+++ Assets/Scripts/ScrollUV.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollUV : MonoBehaviour {
+    private MeshRenderer mr;
+    Material mat;
+    public float parralax = 2f;
+	// Use this for initialization
+	void Start () {
+        mr = GetComponent<MeshRenderer>();
+        mat = mr.material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 offset = mat.mainTextureOffset;
+        offset.x = transform.position.x / transform.localScale.x / parralax;
+        offset.y = transform.position.y / transform.localScale.y / parralax;
+        mat.mainTextureOffset = offset;
+	}
+}
